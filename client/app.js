b8175dc67d2db35b0181dc5d14f7b67ff958a506
hong <gmm117@naver.com>
2020-07-12 22:33:51 +0900

    xmlhttprequest get/post, ajax, axios, fetch example update

--- client/app.js
+++ client/app.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
+const axios = require('axios');
+const fetch = require('node-fetch');
+
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+var $ = jQuery = require('jquery')(window);
 
 app.use(express.static(__dirname + '/public'));
 
+// html get request
 app.get('/', (req, res) => {
     fs.readFile('./public/index.html', function(error, data) {
         if(error) {
@@ -16,6 +24,7 @@ app.get('/', (req, res) => {
 });
 
 // 직접 서버간의 통신으로는 cors error가 발생하지 않는다(브라우저 정책상 제한:domain,port,protocol)
+// XMLHttpRequest get request
 app.get('/serverTest', (req, res) => {
     var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     var xmlHttp = new XMLHttpRequest();
@@ -25,10 +34,74 @@ app.get('/serverTest', (req, res) => {
         }
     }
 
-    xmlHttp.open("GET", "http://localhost:3000/corsTest");
+    xmlHttp.open("GET", "http://localhost:3000/corsTest?" + "id=gmm117&pw=hongseungah");
 
     xmlHttp.send();
     res.send('cors server success');
+});
+
+// XMLHttpRequest post request
+app.get('/serverpostTest', (req, res) => {
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.status == 200 && this.readyState == this.DONE) {
+        console.log(xhr.responseText);
+        }
+    }
+
+    xhr.open("POST", "http://localhost:3000/corspostTest", false);
+    // xhr.setRequestHeader("Content-type", "application/json");
+    // const data = JSON.stringify({
+    //     id: 'gmm117',
+    //     pw: 'hongseungah'
+    // });
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send("id=gmm117&pw=hongseungah");
+    res.send("cors client test");
+});
+
+// ajax post request
+app.get('/serverpostajaxTest', (req, res) => {
+    $.ajax({
+        url:'http://localhost:3000/corspostTest', // 요청 할 주소
+        async:false,// false 일 경우 동기 요청으로 변경
+        type:'POST', // GET, PUT
+        data: "id=gmm117&pw=hongseungah",// 전송할 데이터
+        dataType:'text',// xml, json, script, html
+        beforeSend:function(jqXHR) {},// 서버 요청 전 호출 되는 함수 return false; 일 경우 요청 중단
+        success:function(jqXHR) {},// 요청 완료 시
+        error:function(jqXHR) {},// 요청 실패.
+        complete:function(jqXHR) {}// 요청의 실패, 성공과 상관 없이 완료 될 경우 호출
+    });
+
+    res.send("cors ajax client test");
+});
+
+
+// axios post request
+app.get('/serverpostaxiosTest', (req, res) => {
+    axios({
+        url: 'http://localhost:3000/corspostTest',
+        method: 'POST',
+        data: "id=gmm117&pw=hongseungah"
+      });
+
+    res.send("cors axios client test");
+});
+
+app.get('/serverpostfetchTest', (req, res) => {
+    fetch('http://localhost:3000/corspostTest', {
+        method: 'POST',
+        body: JSON.stringify({
+                id: 'gmm117',
+                pw: 'hongseungah'
+            }),
+        headers: {'Content-Type': 'application/json'}
+    })
+    .then(response => console.log(response));
+
+    res.send("cors fetch client test");
 });
 
 var port = 5000;
--- server/app.js
+++ server/app.js
@@ -1,14 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.get('/', (req, res) => {
-    
     res.send('hello world');
 });
 
 app.get('/corsTest', (req, res) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:5000");
+    
     res.send('cors success');
+    console.log(req.query.id , req.query.pw);
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.post('/corspostTest', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5000");
+    res.send('cors post success');
+    console.log(req.body);
 });
 
 var port = 3000;
